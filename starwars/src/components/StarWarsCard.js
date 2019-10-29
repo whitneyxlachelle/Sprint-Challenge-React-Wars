2668e2d2ea2ccfdb06183cb16839387b1018a756
whitneyxlachelle <whiggins16@yahoo.com>
2019-10-28 20:24:54 -0500

    questions are answered

--- starwars/src/components/StarWarsCard.js
+++ starwars/src/components/StarWarsCard.js
@@ -1,18 +1,19 @@
 import React from "react";
-import { Card, CardTitle, Col } from 'reactstrap';
-
+import { Container, Card, CardTitle, Row, Col } from 'reactstrap';
 
 const StarWarsCard = (props) => {
     return (
-        <div>
-            <Col sm={{ size: 'auto', offset: 1 }}>
-                <Card body outline color="secondary">
-                    <CardTitle>{props.name}</CardTitle>
-                    <CardTitle>{props.gender}</CardTitle>
-                    <CardTitle>{props.year}</CardTitle>
-                </Card>
-            </Col>
-        </div>
+        <Container>
+            <Row>
+                <Col xs="6" sm="4">
+                    <Card body outline color="danger">
+                        <CardTitle>{props.name}</CardTitle>
+                        <CardTitle>{props.gender}</CardTitle>
+                        <CardTitle>{props.year}</CardTitle>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
 
         /* <div className = "star-wars-card" key={props.index}>
    <h1>{props.name}</h1>
